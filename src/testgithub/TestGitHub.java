1a08ed395049d248b8961ca2470196a3242087c2
ThuPN <User@SE130153>
2019-03-24 20:55:26 +0700

    initialize Git Project

--- src/testgithub/TestGitHub.java
+++ src/testgithub/TestGitHub.java
@@ -15,7 +15,7 @@ public class TestGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Test Git Hub");
+        System.out.println("Test Git Hubaaaaaa");
     }
     
 }
